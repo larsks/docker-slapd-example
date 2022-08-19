5d87f3c772bce618ed33e4d37c8fac049ed329a7
Lars Kellogg-Stedman <lars@oddbit.com>
2022-08-18 22:52:59 -0400

    Work on cn=config based configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,55 +1,37 @@
 #!/bin/sh -x
 
-: ${LDAP_DOMAIN:=example.com}
-: ${LDAP_DATA_DIRECTORY:=/var/lib/openldap/data}
-: ${LDAP_RUN_DIRECTORY:=/run/openldap}
 : ${LDAP_LIMIT_FILES:=4096}
-: ${LDAP_LDAPI_SOCKET_PATH:=/run/openldap/ldap}
-: ${LDAP_DEBUG_LEVEL:=1}
-: ${LDAP_LDAPI_SOCKET_URI="ldapi://$(echo "$LDAP_LDAPI_SOCKET_PATH" | sed s,/,%2f,g)"}
-: ${LDAP_URIS:="ldap:/// ldaps:/// ${LDAP_LDAPI_SOCKET_URI}"}
 
-export LDAP_DOMAIN LDAP_DATA_DIRECTORY LDAP_RUN_DIRECTORY
-export LDAP_URIS LDAP_DEBUG_LEVEL
-export LDAP_LDAPI_SOCKET_PATH LDAP_LDAPI_SOCKET_URI
+ulimit -n "${LDAP_LIMIT_FILES}"
 
-if [ -z "$LDAP_ROOT_PASSWORD" ]; then
-	echo "ERROR: Missing LDAP_ROOT_PASSWORD" >&2
-	exit 1
-fi
+install -d -m 700 -o ldap -g ldap /var/lib/openldap/run
+chown -R ldap:ldap /var/lib/openldap
 
-install -d -m 700 -o ldap -g ldap "${LDAP_RUN_DIRECTORY}"
-install -d -m 700 -o ldap -g ldap "${LDAP_DATA_DIRECTORY}"
-
-ldap_suffix=$(echo "$LDAP_DOMAIN" | sed 's/\([^.]*\).\?/dc=\1,/g' | sed 's/,$//')
-
-if ! [ -f /etc/openldap/slapd.d ]; then
+if ! [ -d /etc/openldap/slapd.d ]; then
 	mkdir /etc/openldap/slapd.d
 
-	cat > /etc/openldap/slapd.conf.init <<-EOF
-	include         /etc/openldap/schema/core.schema
+	cat <<-EOF > /etc/openldap/slapd.conf.init
+	pidfile /var/lib/openldap/run/slapd.pid
 
-	pidfile         ${LDAP_RUN_DIRECTORY}/slapd.pid
-	argsfile        ${LDAP_RUN_DIRECTORY}/slapd.args
-
-	modulepath      /usr/lib/openldap
-	moduleload      back_mdb.so
-	database        config
-	rootdn          cn=manager,cn=config
-	rootpw		${LDAP_ROOT_PASSWORD}
-	database        mdb
-	maxsize         1073741824
-	suffix          "${ldap_suffix}"
-	rootdn          "cn=manager,${ldap_suffix}"
-	rootpw          ${LDAP_ROOT_PASSWORD}
-	directory       ${LDAP_DATA_DIRECTORY}
-	index   objectClass     eq
+	database config
+	access to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" manage
 	EOF
 
 	slaptest -f /etc/openldap/slapd.conf.init -F /etc/openldap/slapd.d
+	slapd -h ldapi:///
 
+	ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/core.ldif
+	for ldif in /docker-entrypoint.d/cn=config/*.ldif; do
+		[ -f "$ldif" ] || continue
+		if ! ldapadd -Y EXTERNAL -H ldapi:// -f "$ldif"; then
+			echo "ERROR: failed to process $ldif" >&2
+			exit 1
+		fi
+	done
+
+	kill $(cat /var/lib/openldap/run/slapd.pid)
 	chown -R ldap:ldap /etc/openldap/slapd.d
+
 fi
 
-ulimit -n "${LDAP_LIMIT_FILES}"
 exec "$@"
--- start-slapd.sh
+++ start-slapd.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+: ${LDAP_DEBUG_LEVEL:=0}
+: ${LDAP_URIS:="ldap:// ldaps:// ldapi://"}
+
 exec /usr/sbin/slapd -u ldap \
-	${LDAP_DEBUG_LEVEL:+-d${LDAP_DEBUG_LEVEL}} \
+	-d${LDAP_DEBUG_LEVEL} \
 	-h "${LDAP_URIS}" \
 	${LDAP_EXTRA_ARGS}
