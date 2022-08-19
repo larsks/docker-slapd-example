fca2836be811039e5b92945d5154124e6f16cb4b
Lars Kellogg-Stedman <lars@oddbit.com>
2022-08-18 23:56:46 -0400

    Largely working initialization

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,10 @@ chown -R ldap:ldap /var/lib/openldap
 if ! [ -d /etc/openldap/slapd.d ]; then
 	mkdir /etc/openldap/slapd.d
 
+	# Initial slapd with a configuration that will write a PID file
+	# to a known location, and permit root access to cn=config over
+	# an ldapi:// connection. This will permit us to start up a temporary
+	# slapd instance in order to submit initialization files.
 	cat <<-EOF > /etc/openldap/slapd.conf.init
 	pidfile /var/lib/openldap/run/slapd.pid
 
@@ -17,20 +21,50 @@ if ! [ -d /etc/openldap/slapd.d ]; then
 	access to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth" manage
 	EOF
 
+	# Convert the stub slapd.conf into a cn=config directory configuration.
 	slaptest -f /etc/openldap/slapd.conf.init -F /etc/openldap/slapd.d
+
+	# Start a temporary slapd instance in the background.
 	slapd -h ldapi:///
 
+	# Import the core schema
 	ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/core.ldif
-	for ldif in /docker-entrypoint.d/cn=config/*.ldif; do
-		[ -f "$ldif" ] || continue
-		if ! ldapadd -Y EXTERNAL -H ldapi:// -f "$ldif"; then
-			echo "ERROR: failed to process $ldif" >&2
+
+	for initfile in /docker-entrypoint.d/*; do
+		[ -f "$initfile" ] || continue
+
+		rc=0
+
+		case "$initfile" in
+			*.sh)	sh "$initfile"
+				rc=$?
+				;;
+
+			*.ldif)	ldapadd -Y EXTERNAL -H ldapi:// -f "$initfile"
+				rc=$?
+				;;
+
+			*.schema)
+				schemaname="$(cat initfile)"
+				ldapadd -Y EXTERNAL -H ldapi:// -f "/etc/openldap/schema/$schemaname.ldif"
+				rc=$?
+				;;
+
+			*)	echo "Unsupported extension: $initfile"
+				rc=1
+				;;
+		esac
+		
+		if [ "$rc" -ne 0 ]; then
+			echo "FAILED: $initfile" >&2
 			exit 1
+		else
+			echo "SUCCESS: $initfile"
 		fi
 	done
 
 	kill $(cat /var/lib/openldap/run/slapd.pid)
-	chown -R ldap:ldap /etc/openldap/slapd.d
+	chown -R ldap:ldap /etc/openldap/slapd.d /var/lib/openldap
 
 fi
 
